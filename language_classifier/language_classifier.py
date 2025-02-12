c8e1f991187860bd36230a91588fa5ad35de288c
Kevin <kevincarr@gmail.com>
2025-02-12 13:43:16 -0400

    update chunking script to confirm optimal clf params

--- language_classifier/language_classifier.py
+++ language_classifier/language_classifier.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 class LanguageClassifier:
-    def __init__(self, wordlist_path = None):
+    def __init__(self, wordlist_path=None, max_incorrect_words=0, min_words_in_language=2):
         default_path = Path(__file__).parent / "wordlists.json"
         wordlist_path = wordlist_path or default_path
 
@@ -12,16 +12,20 @@ class LanguageClassifier:
 
         self.french_words = set(wordlists["fr"])
         self.english_words = set(wordlists["en"])
+        self.max_incorrect_words = max_incorrect_words
+        self.min_words_in_language = min_words_in_language
 
-    def classify(self, sentence, max_incorrect_words=0, min_words_in_language=2):
+    def classify(self, sentence):
         words = sentence.lower().split()
         en_count = sum(1 for word in words if word in self.english_words)
         fr_count = sum(1 for word in words if word in self.french_words)
 
-        if en_count > fr_count and en_count >= min_words_in_language and fr_count <= max_incorrect_words:
+        if en_count > fr_count and en_count >= self.min_words_in_language and fr_count <= self.max_incorrect_words:
             return 'en'
-        if fr_count > en_count and fr_count >= min_words_in_language and en_count <= max_incorrect_words:
+        if fr_count > en_count and fr_count >= self.min_words_in_language and en_count <= self.max_incorrect_words:
             return 'fr'
-        if fr_count >= max_incorrect_words and en_count >= max_incorrect_words and fr_count + en_count >= min_words_in_language:
+        if (fr_count >= self.max_incorrect_words and en_count >= self.max_incorrect_words and
+                fr_count + en_count >= self.min_words_in_language):
             return 'mixed'
+
         return 'unknown'
